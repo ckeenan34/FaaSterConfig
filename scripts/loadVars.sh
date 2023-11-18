186b912e3188d12c5303040d20d02076617d7809
Cameron Keenan <cameron.keenan@c3.ai>
2023-11-18 12:21:05 -0500

    Added matmul2 for 2nd cluster (and updated loadVars accordingly)

--- scripts/loadVars.sh
+++ scripts/loadVars.sh
@@ -1,6 +1,13 @@
-export PASSWORD=$(kubectl -n openfaas get secret basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode) && \
-echo "OpenFaaS admin password: $PASSWORD"
+aws eks update-kubeconfig --region us-east-1 --name ccc-cluster
 
 export OPENFAAS_URL=$(kubectl get svc -n openfaas gateway-external -o  jsonpath='{.status.loadBalancer.ingress[*].hostname}'):8080  \
 && echo Your gateway URL is: $OPENFAAS_URL
 
+export PASSWORD=$(kubectl -n openfaas get secret basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode) && \
+echo "OpenFaaS admin password: $PASSWORD"
+
+aws eks update-kubeconfig --region us-east-1 --name ccc-cluster2
+
+export OPENFAAS_URL2=$(kubectl get svc -n openfaas gateway-external -o  jsonpath='{.status.loadBalancer.ingress[*].hostname}'):8080  && echo Your gateway URL is: $OPENFAAS_URL
+export PASSWORD2=$(kubectl -n openfaas get secret basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode) && \
+echo "OpenFaaS admin password: $PASSWORD2"
