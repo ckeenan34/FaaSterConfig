5e330200c24f402c93a7ea491ad9cf051756e1a3
Cameron Keenan <cameron.keenan@c3.ai>
2023-11-19 22:06:37 -0500

    Remaining runs completed

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -5,24 +5,24 @@ lArgs="-c 0.1 0.3 0.5 0.7 0.9 1.1 1.3 1.5 1.7 1.9 -m 256.0 768.0 1280.0 1792.0 2
 mArgs="-c 0.1 0.5 0.9 1.3 1.7 -m 256.0 1280.0 2304.0 3328.0"
 sArgs="-c 0.1 0.9 1.7 -m 256.0 2304.0"
 
-extraArgs=" -to 240 -con 10"
+extraArgs=" -to 240 -con 5"
 matmulInputs=(10 1000 5000)
 
 set -o xtrace
-for mi in "${matmulInputs[@]}";
-do
-  # ./FaaSterConfig.py ../openFaas/matmul2.yml -d $mi $sArgs $extraArgs #complete
-  # ./FaaSterConfig.py ../openFaas/matmul2.yml -d $mi $mArgs $extraArgs #complete
-  ./FaaSterConfig.py ../openFaas/matmul2.yml -d $mi $lArgs $extraArgs
-  # ./FaaSterConfig.py ../openFaas/matmul2.yml -d $mi $xlArgs $extraArgs
-done
+# for mi in "${matmulInputs[@]}";
+# do
+#   # ./FaaSterConfig.py ../openFaas/matmul2.yml -d $mi $sArgs $extraArgs #complete
+#   # ./FaaSterConfig.py ../openFaas/matmul2.yml -d $mi $mArgs $extraArgs #complete
+#   # ./FaaSterConfig.py ../openFaas/matmul2.yml -d $mi $lArgs $extraArgs
+#   # ./FaaSterConfig.py ../openFaas/matmul2.yml -d $mi $xlArgs $extraArgs
+# done
 
 # ./FaaSterConfig.py ../openFaas/image_processing.yml -d https://raw.githubusercontent.com/MBtech/rethinking-serverless/main/benchmarks/face-detection/pigo-openfaas/samples/nasa.jpg $mArgs $extraArgs #complete
 # ./FaaSterConfig.py ../openFaas/image_processing.yml -d https://raw.githubusercontent.com/MBtech/rethinking-serverless/main/benchmarks/face-detection/pigo-openfaas/samples/nasa.jpg $sArgs $extraArgs #complete
-# ./FaaSterConfig.py ../openFaas/ocr.yml -d https://www.pyimagesearch.com/wp-content/uploads/2017/06/tesseract_header.jpg $mArgs $extraArgs
-# ./FaaSterConfig.py ../openFaas/s3.yml -d '{"input_bucket": "inputbucketbenchmark","object_key": "amzn_fine_food_reviews/reviews100mb.csv","output_bucket":"outputbucketbenchmark"}' $mArgs $extraArgs
-# ./FaaSterConfig.py ../openFaas/ocr.yml -d https://www.pyimagesearch.com/wp-content/uploads/2017/06/tesseract_header.jpg $sArgs $extraArgs
-# ./FaaSterConfig.py ../openFaas/s3.yml -d '{"input_bucket": "inputbucketbenchmark","object_key": "amzn_fine_food_reviews/reviews100mb.csv","output_bucket":"outputbucketbenchmark"}' $sArgs $extraArgs
+./FaaSterConfig.py ../openFaas/ocr.yml -d https://www.pyimagesearch.com/wp-content/uploads/2017/06/tesseract_header.jpg $mArgs $extraArgs
+./FaaSterConfig.py ../openFaas/s3.yml -d '{"input_bucket": "inputbucketbenchmark","object_key": "amzn_fine_food_reviews/reviews100mb.csv","output_bucket":"outputbucketbenchmark"}' $mArgs $extraArgs
+./FaaSterConfig.py ../openFaas/ocr.yml -d https://www.pyimagesearch.com/wp-content/uploads/2017/06/tesseract_header.jpg $sArgs $extraArgs
+./FaaSterConfig.py ../openFaas/s3.yml -d '{"input_bucket": "inputbucketbenchmark","object_key": "amzn_fine_food_reviews/reviews100mb.csv","output_bucket":"outputbucketbenchmark"}' $sArgs $extraArgs
 
 set +o xtrace
 
